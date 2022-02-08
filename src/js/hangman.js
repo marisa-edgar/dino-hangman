e85f6b78c4646dce4141735743e6bb7cd8423e38
Anastasiia Riabets <anastasiia.ria@gmail.com>
2022-02-08 14:41:21 -0800

    add api call
    Co-authored-by: S-Espinet <s.lou.espinet@gmail.com>
    Co-Authored by: Marisa Edgar <marisaedgar1212@gmail.com>

--- src/js/hangman.js
+++ src/js/hangman.js
@@ -1,3 +1,17 @@
 export default class Hangman {
-  constructor() {}
+  static getDino() {
+    return new Promise(function (resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = "https://dinoipsum.com/api/?format=text&words=1&paragraphs=1";
+      request.onload = function () {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(request.response);
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
 }
